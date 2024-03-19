f7fe743e3c1a8c3003c381353be95c1e7046bf51
horun.fujimoto <hello.horurun@gmail.com>
2024-03-19 08:21:49 +0000

    [ADD]card.blade.php

---
+++ resources/views/users/card.blade.php
@@ -0,0 +1,9 @@
+<div class="card border border-base-300 rounded-lg" style="width: fit-content;">
+    <div class="card-body bg-base-200 text-4xl">
+        <h2 class="card-title">{{ $user->name }}</h2>
+    </div>
+    <figure>
+        {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+        <img src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+    </figure>
+</div>

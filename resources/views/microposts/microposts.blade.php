db2dd36529aaf211f124a61a0f1d58ea73675adf
horun.fujimoto <hello.horurun@gmail.com>
2024-03-21 02:42:51 +0000

    [ADD]microposts.blade.php

---
+++ resources/views/microposts/microposts.blade.php
@@ -0,0 +1,29 @@
+<div class="mt-4">
+    @if (isset($microposts))
+        <ul class="list-none">
+            @foreach ($microposts as $micropost)
+                <li class="flex items-start gap-x-2 mb-4">
+                    {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                    <div class="avatar">
+                        <div class="w-12 rounded">
+                            <img src="{{ Gravatar::get($micropost->user->email) }}" alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
+                            <a class="link link-hover text-info" href="{{ route('users.show', $micropost->user->id) }}">{{ $micropost->user->name }}</a>
+                            <span class="text-muted text-gray-500">posted at {{ $micropost->created_at }}</span>
+                        </div>
+                        <div>
+                            {{-- 投稿内容 --}}
+                            <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        {{-- ページネーションのリンク --}}
+        {{ $microposts->links() }}
+    @endif
+</div>

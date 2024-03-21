7d859a5f3c43f631eb822dd9c78d425b05462351
horun.fujimoto <hello.horurun@gmail.com>
2024-03-21 03:14:08 +0000

    [ADD]dashboard.blade.php追記

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,26 +2,22 @@
 
 @section('content')
     @if (Auth::check())
-        <div class="sm:grid sm:grid-cols-3 sm:gap-10">
-            <aside class="mt-4">
+        <div class="row">
+            <aside class="col-sm-4">
                 {{-- ユーザ情報 --}}
                 @include('users.card')
             </aside>
-            <div class="sm:col-span-2">
+            <div class="col-sm-8">
                 {{-- 投稿フォーム --}}
-                
+                @include('microposts.form')
                 {{-- 投稿一覧 --}}
                 @include('microposts.microposts')
             </div>
         </div>
     @else
-        <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
-            <div class="hero-content text-center my-10">
-                <div class="max-w-md mb-10">
-                    <h2>Welcome to the Microposts</h2>
-                    {{-- ユーザ登録ページへのリンク --}}
-                    <a class="btn btn-primary btn-lg normal-case" href="{{ route('register') }}">Sign up now!</a>
-                </div>
+        <div class="center jumbotron">
+            <div class="navbar-brand d-flex flex-row-reverse bd-highlight">
+                Welcome to the Microposts
             </div>
         </div>
     @endif
